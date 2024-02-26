314a14124c1029af20933570dc6453ec7dba39de
LuizCowBTF <luizcowbtf@gmail.com>
2024-02-26 11:33:28 -0300

    saparate rooms

--- public/socket-front-document.js
+++ public/socket-front-document.js
@@ -2,17 +2,19 @@ import { atualizaTextoEditor } from "./documento.js";
 
 const socket = io();
 
-function emitirTextoEditor(texto) {
-  socket.emit("texto_editor", texto);
+function selecionarDocumento(nome) {
+  socket.emit("selecionar_documento", nome);
+}
+
+function emitirTextoEditor(dados) {
+  socket.emit("texto_editor", dados);
 }
 
 socket.on("texto_editor_clientes", (texto) => {
   atualizaTextoEditor(texto);
 });
 
-function selecionarDocumento(nome) {
-  socket.emit("selecionar_documento", nome);
-}
+
 
 
 export { emitirTextoEditor, selecionarDocumento };
